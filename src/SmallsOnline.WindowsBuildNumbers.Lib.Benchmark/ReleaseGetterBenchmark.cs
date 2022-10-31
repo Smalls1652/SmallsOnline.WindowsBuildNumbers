a67d2565bcdded74784e6763e0bfc25c9c04a6a1
Timothy Small <smalls.online@icloud.com>
2022-10-31 11:28:56 -0400

    Add Benchmark project

---
+++ src/SmallsOnline.WindowsBuildNumbers.Lib.Benchmark/ReleaseGetterBenchmark.cs
@@ -0,0 +1,27 @@
+using BenchmarkDotNet.Attributes;
+using SmallsOnline.WindowsBuildNumbers.Lib.Helpers;
+using SmallsOnline.WindowsBuildNumbers.Lib.Models;
+
+namespace SmallsOnline.WindowsBuildNumbers.Lib.Benchmark;
+
+[MemoryDiagnoser(displayGenColumns: true)]
+public class ReleaseGetterBenchmark
+{
+    private readonly ReleaseInfoGetter _releaseInfoGetter;
+
+    public ReleaseGetterBenchmark()
+    {
+        _releaseInfoGetter = new();
+    }
+
+    public ReleaseGetterBenchmark(ReleaseInfoGetter releaseInfoGetter)
+    {
+        _releaseInfoGetter = releaseInfoGetter;
+    }
+
+    [Benchmark]
+    public async Task<List<ReleaseInfo>> GetReleaseInfoWindows10_Benchmark() => await _releaseInfoGetter.GetWindowsReleaseInfoAsync(MajorWindowsVersion.Windows10);
+
+    [Benchmark]
+    public async Task<List<ReleaseInfo>> GetReleaseInfoWindows11_Benchmark() => await _releaseInfoGetter.GetWindowsReleaseInfoAsync(MajorWindowsVersion.Windows11);
+}
