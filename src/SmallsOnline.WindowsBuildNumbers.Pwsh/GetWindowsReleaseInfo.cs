c55679dbdd7fd650b7308f664469c8c26861f1e3
Timothy Small <smalls.online@icloud.com>
2022-11-08 22:09:17 -0500

    Add Get-WindowsReleaseInfo

---
+++ src/SmallsOnline.WindowsBuildNumbers.Pwsh/GetWindowsReleaseInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Management.Automation;
+using SmallsOnline.WindowsBuildNumbers.Lib.Helpers;
+using SmallsOnline.WindowsBuildNumbers.Lib.Models;
+
+namespace SmallsOnline.WindowsBuildNumbers.Pwsh;
+
+[Cmdlet(verbName: VerbsCommon.Get, nounName: "WindowsReleaseInfo")]
+public class GetWindowsBuildNumbers : Cmdlet
+{
+    [Parameter(Mandatory = false, Position = 0)]
+    public MajorWindowsVersion WindowsVersion { get; set; } = MajorWindowsVersion.Windows10;
+
+    protected override void BeginProcessing()
+    {
+        base.BeginProcessing();
+    }
+
+    protected override void ProcessRecord()
+    {
+        ReleaseInfoGetter releaseInfoGetter = new();
+
+        WriteVerbose($"Getting release info for '{WindowsVersion}'.");
+        ReleaseInfo[] releases = releaseInfoGetter.GetWindowsReleaseInfoAsync(
+            windowsVersion: WindowsVersion
+        ).GetAwaiter().GetResult();
+
+        foreach (ReleaseInfo releaseItem in releases)
+        {
+            WriteObject(releaseItem);
+        }
+
+        releaseInfoGetter.Dispose();
+    }
+
+    protected override void StopProcessing()
+    {
+        base.StopProcessing();
+    }
+}
